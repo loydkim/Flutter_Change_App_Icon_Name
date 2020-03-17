dfce6da2a55a4f65dc7df63052716099f3ed72f2
Loydkim <kimloydmail@gmail.com>
2020-03-16 20:39:12 -0400

    Add files via upload

---
+++ android/app/src/main/java/test/name/icon/app/change/example/flutter/com/changeappiconandname/MainActivity.java
@@ -0,0 +1,13 @@
+package test.name.icon.app.change.example.flutter.com.changeappiconandname;
+
+import android.support.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+    GeneratedPluginRegistrant.registerWith(flutterEngine);
+  }
+}
